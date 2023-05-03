a67f72833108beef7ea8b2d18a93929ff86e5bf6
Isaiah Youm <isaiahyoum.coding@gmail.com>
2023-05-03 15:38:40 -0700

    Finished integrating the OpenAI API to accept requests at the URL '/v1/chat/test' and generate texts. URL specified is for test purposes.

---
+++ server.js
@@ -0,0 +1,11 @@
+// All things to do with servers are put here.
+
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
+const app = require('./src/middleware/app');
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+})
---
+++ src/middleware/app.js
@@ -0,0 +1,23 @@
+// This page is EXPRESS / MIDDLEWARE Only
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+const botRouter = require(`${__dirname}/routes/botRouter`);
+
+// Morgan is a development dependency that allows us to visualize status codes better
+// for API tests. 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use('/v1/chat/test', botRouter);
+
+module.exports = app;
