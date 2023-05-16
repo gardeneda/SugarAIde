4ac7e1be9afb54a963ce1673820cc6ef65353882
Isaiah Youm <isaiahyoum.coding@gmail.com>
2023-05-16 11:03:14 -0700

    Separated the login/signup/logout buttons from the main content of the navbar.

--- src/utils/navLinkManager.js
+++ src/utils/navLinkManager.js
@@ -1,5 +1,4 @@
 const navLinks = [
-    { name: "Home", link: "/" },
     { name: "Profile", link: "/profile" },
     { name: "Exercise", link: "/exercisePage" },
     { name: "Diet", link: "/checkCalories"},
