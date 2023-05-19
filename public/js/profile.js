01dd994e8c35fe3e84f60a264bee808e8e30e216
Ran Park <kpark90@my.bcit.ca>
2023-05-18 23:58:06 -0700

    Implemented calorie/nutrtion track and some other minor feature and css

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
     $.post("/profile/updateHealthInfo", {weight: newWeight}, function(data){
       if(data=='success'){
         alert("Weight updated successfully!");
+      }else {
+        console.log('Failed to update activity');
       }
     });
   });
@@ -13,8 +15,25 @@ $(document).ready(function(){
     $.post("/profile/updateHealthInfo", {age: newAge}, function(data){
       if(data=='success'){
         alert("Age updated successfully!");
+      }else {
+        console.log('Failed to update activity');
       }
     });
   });
 });
 
+$(document).ready(function() {
+  // Update Activity Button Click Event
+  $('#update-activity-button').on('click', function() {
+    var newActivity = $('#activity-select').val();
+
+    // Send a POST request to update activity
+    $.post("/profile/updateHealthInfo", { activity: newActivity }, function(data) {
+      if (data === 'success') {
+        alert("Activity updated successfully!");
+      } else {
+        console.log('Failed to update activity');
+      }
+    });
+  });
+});
