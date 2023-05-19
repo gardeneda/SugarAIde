9eba76e84c341ec5f47c1489a5b6937c651e3ed2
Isaiah Youm <isaiahyoum.coding@gmail.com>
2023-05-19 04:25:56 -0700

    Added validation to check if To-Do list for the current date is produced for the user when they reach the main page. If they do not have one, generate it here.

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -2,12 +2,13 @@ const express = require("express");
 const validation = require(`${__dirname}/../utils/validation`);
 
 const mainController = require(`${__dirname}/../controllers/mainController`);
+const todoController = require(`${__dirname}/../controllers/todoController`);
 
 const router = express.Router();
 
 router
   .route("/")
-  .get(mainController.createHTML);
+  .get(todoController.generateToDoList, mainController.createHTML);
   
 router
   .route('/exerciseData')
