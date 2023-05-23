8df2233a74159ec25915db92ea278b2705ffad42
Isaiah Youm <isaiahyoum.coding@gmail.com>
2023-05-23 12:26:41 -0700

    Added new field in data schema to universalize the date, and implemented function to retrieve all exercises for a specified date.

--- src/controllers/dailyReportController.js
+++ src/controllers/dailyReportController.js
@@ -1,2 +1,38 @@
 // TODO: Create nurtrition and exercise data today.
 // TODO: Find a way to pull data from nutritionLog and exerciseLog for yesterday
+
+/* ///////////////////////////////////////////////// */
+/*  Required Packages and Constant Declaration */
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+const database = require(`${__dirname}/../config/databaseConfig`);
+const userCollection = database
+	.db(process.env.MONGODB_DATABASE)
+	.collection("users");
+
+/* End of Required Packages and Constant Declaration */
+/* ///////////////////////////////////////////////// */
+
+exports.getExercisesByDate = async (account, date) => {
+    const pipeline = [
+      { $match: { email: account } },
+      {
+        $project: {
+          exerciseLog: {
+            $filter: {
+              input: "$exerciseLog",
+              as: "exercise",
+              cond: { $eq: ["$$exercise.date_real", date] }
+            }
+          }
+        }
+      }
+    ];
+
+    const result = await userCollection.aggregate(pipeline).toArray();
+    return result.length > 0 ? result[0].exerciseLog : [];
+};
+
+
+  
