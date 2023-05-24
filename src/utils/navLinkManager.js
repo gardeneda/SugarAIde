8f2dedc335c3f1d9d01ca8eab0e3cb3823f33e49
Hiroshi <mnakasone3@my.bcit.ca>
2023-05-23 18:27:56 -0700

    Fixed issue with email limit to 20 chars, fixed css, added pattern for input fields in exerciseForm

--- src/utils/navLinkManager.js
+++ src/utils/navLinkManager.js
@@ -1,11 +1,11 @@
 const navLinks = [
-    { name: "Profile", link: "/profile" },
-    { name: "Exercise", link: "/exercisePage" },
-    { name: "Diet", link: "/dietTrack" },
-    { name: "Risk", link: "/risk" },
     { name: "Chat", link: "/chat" },
-    { name: "Resources", link: "/additionalInfo" },
-    { name: "To-Do", link: "/todo" }
+    { name: "Diet", link: "/dietTrack" },
+    { name: "Exercise", link: "/exercisePage" },
+    { name: "To-Do", link: "/todo" },
+    { name: "Profile", link: "/profile" },
+    { name: "Risk", link: "/risk" },
+    { name: "Resources", link: "/additionalInfo" }
   ];
   
 function highlightCurrentLink(pathname) {
