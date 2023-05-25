0674c617a3d5d51e2e866da0f5d5e58aa57f389d
Isaiah Youm <isaiahyoum.coding@gmail.com>
2023-05-25 03:39:17 -0700

    Implemented a script that will run at 12:00AM to reset status checks for all users in the database.

--- server.js
+++ server.js
@@ -5,7 +5,30 @@ dotenv.config({ path: './.env' });
 
 const app = require('./src/middleware/app');
 
+const database = require(`${__dirname}/src/config/databaseConfig`);
+const userCollection = database
+	.db(process.env.MONGODB_DATABASE)
+    .collection("users");
+    
+const schedule = require('node-schedule');
+const todoController = require('./src/controllers/todoController');
+const dailyReportController = require('./src/controllers/dailyReportController');
+
 const port = process.env.PORT || 5050;
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
 })
+
+const job = schedule.scheduleJob('0 38 * * * *', async () => {
+    // Code to be executed at 12:00 AM every day
+    console.log("The script has successfully run at its designated time.");
+    
+    await dailyReportController.resetReport();
+
+    const allUsers = await userCollection.find({}).toArray();
+    allUsers.forEach(async (user) => {
+        await todoController.generateToDoListScript(user.email, user);
+    })
+    
+    console.log("Script has finished running.");
+});
