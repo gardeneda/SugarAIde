8fb9f287d5ac0807a58c30676720bb77da4ea408
Isaiah Youm <isaiahyoum.coding@gmail.com>
2023-05-26 12:05:56 -0700

    Fixed the health information form to prevent double-clicks and properly send data over.

--- public/js/healthinfo.js
+++ public/js/healthinfo.js
@@ -10,40 +10,54 @@ const form = document.querySelector(".needs-validation");
 
 // Add submit event listener to the form
 form.addEventListener("submit", async function (event) {
-    // Prevent form submission
-    event.preventDefault();
-    event.stopPropagation();
+	// Prevent form submission
+	event.preventDefault();
+	event.stopPropagation();
 
-    // Check if the form is valid
-    if (form.checkValidity()) {
-        // Add is-valid class to the form
-        form.classList.add("is-valid");
-        loader.style.display = 'block';
+	// Check if the form is valid
+	if (form.checkValidity()) {
+		// Add is-valid class to the form
+		form.classList.add("is-valid");
+		loader.style.display = "block";
 
-    } else {
-        // Add is-invalid class to the form
-        form.classList.add("is-invalid");
-    }
+		// Create the request object
+		const request = {};
 
-    // Add Bootstrap validation classes to the form fields
-    [...form.elements].forEach(function (element) {
-        if (element.checkValidity()) {
-            element.classList.remove("is-invalid");
-            element.classList.add("is-valid");
+		// Iterate over the form fields
+		[...form.elements].forEach(function (element) {
+			// Exclude the submit button from the request
+			if (element.type !== "submit") {
+				request[element.name] = element.value;
+			}
+		});
 
-        } else {
-            element.classList.remove("is-valid");
-            element.classList.add("is-invalid");
-        }
-    });
-    
-    if (form.checkValidity()) {
-        const response = await fetch("https://drab-rose-indri-sari.cyclic.app/health/form", {
-            method: "post",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(request)
-        });
-    }
+		// Make the form submission
+		const response = await fetch(
+			"https://drab-rose-indri-sari.cyclic.app/health/form",
+			{
+				method: "post",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(request),
+			}
+		);
+
+		window.location.href = "/risk";
+
+	} else {
+		// Add is-invalid class to the form
+		form.classList.add("is-invalid");
+	}
+
+	// Add Bootstrap validation classes to the form fields
+	[...form.elements].forEach(function (element) {
+		if (element.checkValidity()) {
+			element.classList.remove("is-invalid");
+			element.classList.add("is-valid");
+		} else {
+			element.classList.remove("is-valid");
+			element.classList.add("is-invalid");
+		}
+	});
 });
