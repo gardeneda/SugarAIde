0cf8a928d31c68106f257df80003925a2bbc68b8
Isaiah Youm <isaiahyoum.coding@gmail.com>
2023-05-25 19:25:52 -0700

    Inserted missing code in mainRouter.js

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -3,11 +3,13 @@ const validation = require(`${__dirname}/../utils/validation`);
 
 const mainController = require(`${__dirname}/../controllers/mainController`);
 const dailyReportController = require(`${__dirname}/../controllers/dailyReportController`);
+const calorieRequirement = require(`${__dirname}/../controllers/calorieRequirmentController`);
 
 const router = express.Router();
 
 router.route("/")
   .get(dailyReportController.checkFirstLoginOnMain,
+    calorieRequirement.getDailyValues,
     mainController.createHTML);
   
 router.route('/exerciseData')
