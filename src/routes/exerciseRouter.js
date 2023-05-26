54208078971b3ca321c119987f8412af8769a8e5
Hiroshi <mnakasone3@my.bcit.ca>
2023-05-26 16:40:38 -0700

    Added missing comments to functions

--- src/routes/exerciseRouter.js
+++ src/routes/exerciseRouter.js
@@ -4,15 +4,15 @@ const validation = require(`${__dirname}/../utils/validation`);
 const exerciseController = require(`${__dirname}/../controllers/exerciseController`);
 const router = express.Router();
 
-
+//Route to create HTML
 router.route('/')
     .get(validation.checkValidSession, exerciseController.createHTML)
-
+//Route to get data
 router.route('/calendarData')
     .get(exerciseController.getCalendarData);
-
+//Route to delete data
 router.delete('/calendarData/:id', exerciseController.deleteExerciseData);
-
+//Route to update data
 router.put('/calendarData/:id', exerciseController.updateExerciseData);
 
 
